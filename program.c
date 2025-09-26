c74c8c9ed3ca3e01c8741709caa6f480408c1911
Paola-dev26 <simoes.paola21@gmail.com>
2025-09-26 08:06:41 +0000

    new

--- program.c
+++ program.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    double wind_angle;	
+    double wind_speed;
+    double fuel;	
+    double consumption;	
+    double intwing_surface;	
+    double pressure;	
+    int temperature;	
+    int altitude;	
+    double ias;	
+    double weight;
+    printf("Direction relative du vent [-3,14159265 à 3,14159265] : ");
+    scanf("%lf", & wind_angle); 
+
+    printf("Vitesse vent [0 à 150] : ");
+    scanf("%lf", & wind_speed); 
+
+    printf("fuel [20 à 350000] : ");
+    scanf("%lf", & fuel);
+
+    printf("consumption [10 à 15000] : ");
+
+
     return 0;
+
 }
